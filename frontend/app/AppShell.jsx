9696a28b88a13b4795e65064540cfe96b41a62ca
kang <kijong4309@naver.com>
2025-12-23 16:54:46 +0900

    강기종 - 특정 페이지 푸터 제거

--- frontend/app/AppShell.jsx
+++ frontend/app/AppShell.jsx
@@ -5,23 +5,42 @@ import Footer from "@/components/commons/Footer";
 import ScrollToTop from "@/components/commons/ScrollToTop";
 import AuthHydrator from "@/app/providers/AuthHydrator.js";
 import { useAuthStore } from "@/store/authStore.js";
+import { usePathname } from "next/navigation";
+
 export default function AppShell({ children }) {
     const authChecked = useAuthStore((s) => s.authChecked);
+    const pathname = usePathname();
+
+    /** Footer 숨길 경로 */
+    const hideFooterPaths = [
+        "/travel",
+        "/travel/",
+        "/rental",
+        "/rental/"
+    ];
+
+    const hideFooter = hideFooterPaths.some((path) =>
+        pathname.startsWith(path)
+    );
+
     return (
         <>
             <AuthHydrator />
 
             {!authChecked ? (
                 <div className="app-loading">
-                    {/* 로딩 UI 넣기 */}
-                    <p style={{textAlign:"center", paddingTop: "50px"}}>🚀  Loading...</p>
+                    <p style={{ textAlign: "center", paddingTop: "50px" }}>
+                        🚀 Loading...
+                    </p>
                 </div>
             ) : (
                 <>
                     <ScrollToTop />
                     <Header />
                     <main>{children}</main>
-                    <Footer />
+
+                    {/* 조건 처리 */}
+                    {!hideFooter && <Footer />}
                 </>
             )}
         </>
